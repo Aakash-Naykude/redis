a6d9addc44851f48f86ae2b3ace770738781a31b
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-12-11 12:51:31 +0530

    added delete

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -43,24 +43,31 @@ router.get("/:id", (req, res) => {
   }
 });
 
-
-router.patch("/:id", (req, res) => {
+router.patch("/:id", async (req, res) => {
   try {
-    redis.get(`products.${req.params.id}`, async function (err, products) {
-      console.log("products", products);
-      if (err) console.log(err);
-      if (products)
-        return res.status(200).send({ Cashaed_forecast: JSON.parse(products) });
-      const user = await Product.findById(req.params.id).lean().exec();
-      redis.set(`products.${req.params.id}`, JSON.stringify(user));
-      return res.status(200).send({ db_forcast: user });
+    const user = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
     });
+    redis.set(`products.${req.params.id}`, JSON.stringify(user));
+
+    const users = await Product.find().lean().exec();
+    redis.set("products", JSON.stringify(users));
+    return res.status(200).send({ db_forcast: user });
   } catch (e) {
     return res.status(500).json({ status: "Failed", message: e.message });
   }
 });
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await Product.findByIdAndDelete(req.params.id);
+    redis.del(`products.${req.params.id}`);
 
-
-
+    const users = await Product.find().lean().exec();
+    redis.set("products", JSON.stringify(users));
+    return res.status(200).send({ db_forcast: user });
+  } catch (e) {
+    return res.status(500).json({ status: "Failed", message: e.message });
+  }
+});
 
 module.exports = router;
